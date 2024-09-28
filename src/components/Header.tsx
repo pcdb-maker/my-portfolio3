63297aa0938ad54cbeabe8e6bf780414fe179f18
Chloe O <66051807+pcdb-maker@users.noreply.github.com>
2024-09-28 23:07:12 +1000

    const MainHeading = styled.h1`

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,3 +9,10 @@ const HeaderContainer = styled.div`
   font-family: 'Poppins', sans-serif;
 `;
 
+const MainHeading = styled.h1`
+  font-size: 48px; /* Large font size for main heading */
+  color: #e85a4f;
+  font-weight: 700;
+  margin: 0;
+`;
+
