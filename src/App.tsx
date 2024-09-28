90d0cadc29197a12f1e5457c521b2b89439653fa
Chloe O <66051807+pcdb-maker@users.noreply.github.com>
2024-09-28 23:05:47 +1000

    Import

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Import routing components
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Portfolio from './components/Portfolio';
