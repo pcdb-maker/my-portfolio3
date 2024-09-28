7544fbc65189afa5758c04e3783f61ce5f53082c
Chloe O <66051807+pcdb-maker@users.noreply.github.com>
2024-09-28 23:07:07 +1000

    const HeaderContainer = styled.div`

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,3 +2,10 @@
 import React from 'react';
 import styled from 'styled-components';
 
+const HeaderContainer = styled.div`
+  padding: 50px 20px;
+  text-align: center;
+  background-color: #f7f3e9; /* Header background color */
+  font-family: 'Poppins', sans-serif;
+`;
+
