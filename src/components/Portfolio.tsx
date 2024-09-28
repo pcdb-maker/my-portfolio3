ae3d5c908ef1697453eae9f72a0f4efcb6a569ed
Chloe O <66051807+pcdb-maker@users.noreply.github.com>
2024-09-28 23:15:01 +1000

    Cards

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -87,6 +87,19 @@ const Portfolio: React.FC = () => {
         </ProjectCard>
 
  {/* Third Card */}
+ <ProjectCard
+          large={false}
+          style={{ y: y3 }} // Applying the y3 transform value
+        >
+          <Link to="/project2">
+            <ParallaxImage
+              src="https://static.wixstatic.com/media/36e847_9feb77b2a07c4d749bbcca75887be7ee~mv2.webp"
+              alt="project2"
+            />
+          </Link>
+        </ProjectCard>
+
+        {/* Forth Card */}
   );
 };
 
