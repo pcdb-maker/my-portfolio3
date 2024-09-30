a9565eb7085c984421770b69608ec9e6392d3cbc
Chloe O <66051807+pcdb-maker@users.noreply.github.com>
2024-09-30 22:34:27 +1000

    Portfolio header

---
+++ src/components/PortfolioHeader.tsx
@@ -0,0 +1,35 @@
+// PortolioHeader.tsx
+import React from 'react';
+import styled from 'styled-components';
+
+const HeaderContainer = styled.div`
+  padding: 50px 20px;
+  text-align: center;
+  background-color: #f7f3e9; /* Header background color */
+  font-family: 'Poppins', sans-serif;
+`;
+
+const MainHeading = styled.h1`
+  font-size: 48px; /* Large font size for main heading */
+  color: #e85a4f;
+  font-weight: 700;
+  margin: 0;
+`;
+
+const SubHeading = styled.p`
+  font-size: 24px; /* Smaller font size for subheading */
+  color: #4056a1;
+  margin: 10px 0 0;
+`;
+
+const Header: React.FC = () => {
+  return (
+    <HeaderContainer id='header'>
+      <MainHeading>Selected Works</MainHeading>
+      <SubHeading>A showcase of my recent projects and creative endeavors.</SubHeading>
+    </HeaderContainer>
+
+  );
+};
+
+export default Header;
