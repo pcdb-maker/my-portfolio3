b1a67f689699f7d3e3bc3a34367fc8dce9478569
Chloe O <66051807+pcdb-maker@users.noreply.github.com>
2024-10-04 13:02:10 +1000

    {/* Sixth Card */}

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -182,12 +182,18 @@ const Portfolio: React.FC = () => {
           </Link>
         </ProjectCard>
 
-        {/* Sixth Card */}
-        <ProjectCard large={false} style={{ y: y6 }}>
+ {/* sixth Card */}
+ <ProjectCard large={false} style={{ y: y6 }} transition={{ ease: [0.25, 0.1, 0.25, 1], duration: 0.01 }}>
           <Link to="/project2">
             <ParallaxImage src="https://static.wixstatic.com/media/36e847_9feb77b2a07c4d749bbcca75887be7ee~mv2.webp" alt="project2" />
+            <CardTitle>Opere d'Olio</CardTitle>
+            <Subtitle>Packaging Design</Subtitle>
+            <CircleLink className="CircleLink">→</CircleLink>
           </Link>
         </ProjectCard>
+
+
+
       </PortfolioSection>
     </>
   );
