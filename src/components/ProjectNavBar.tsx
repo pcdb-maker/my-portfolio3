46e9934fc61b5ec45ae6ee99c13b2cf099356cc2
Chloe O <66051807+pcdb-maker@users.noreply.github.com>
2024-10-04 13:08:50 +1000

    ProjectNavbarContainer

--- src/components/ProjectNavBar.tsx
+++ src/components/ProjectNavBar.tsx
@@ -3,3 +3,32 @@ import { Link } from 'react-scroll';
 import styled from 'styled-components';
 import { ReactComponent as ClubIcon } from '../assets/club.svg'; // Import Diamond SVG
 
+const ProjectNavbarContainer = styled.nav`
+  top: 0;
+  width: 100%;
+  margin: 0;
+  background: #EFE2BA;; /* Navbar background color */
+  display: flex;
+  justify-content: space-between; /* Aligns items to the left and right */
+  align-items: center;
+  padding: 20px 40px; /* Adjust padding as needed */
+  z-index: 1000; /* Ensures the navbar is always above other elements */
+  transition: background 0.3s;
+  overflow: hidden; /* Prevent overflow */
+  box-sizing: border-box; /* Include padding and border in element width/height */
+
+  /* Adding the underline with a pseudo-element */
+  position: relative;
+  
+  &::after {
+    content: '';
+    position: absolute;
+    bottom: 0;
+    left: 50%; /* Center the underline horizontally */
+    transform: translateX(-50%);
+    width: 1100px; /* Set your preferred underline length */
+    border-bottom: 1.5px solid black; /* The underline */
+  }
+`;
+
+
