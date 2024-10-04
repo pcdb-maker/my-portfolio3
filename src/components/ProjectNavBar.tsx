2542a90f37363beead32a5286b90b8e6077f89b5
Chloe O <66051807+pcdb-maker@users.noreply.github.com>
2024-10-04 13:08:32 +1000

    Imports

---
+++ src/components/ProjectNavBar.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import { Link } from 'react-scroll';
+import styled from 'styled-components';
+import { ReactComponent as ClubIcon } from '../assets/club.svg'; // Import Diamond SVG
+
