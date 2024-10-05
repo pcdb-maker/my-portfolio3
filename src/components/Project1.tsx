1628e6bec156d01400f6e1c9873781d02d3cb8f5
Chloe O <66051807+pcdb-maker@users.noreply.github.com>
2024-10-05 18:52:07 +1000

    Update Project1.tsx

--- src/components/Project1.tsx
+++ src/components/Project1.tsx
@@ -307,6 +307,7 @@ const Project1: React.FC = () => {
         </LiveProjectButton>
       </InfoBox>
 
+
       {/* Image Grid */}
       <ImageGrid>
         <GridImage src="https://i.natgeofe.com/n/548467d8-c5f1-4551-9f58-6817a8d2c45e/NationalGeographic_2572187.jpg?w=1436&h=958" alt="Image 1" />
