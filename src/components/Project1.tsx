eda125d0405ed8427606c0a62d97b2a738840932
Chloe O <66051807+pcdb-maker@users.noreply.github.com>
2024-10-05 18:52:11 +1000

    Update Project1.tsx

--- src/components/Project1.tsx
+++ src/components/Project1.tsx
@@ -307,6 +307,7 @@ const Project1: React.FC = () => {
         </LiveProjectButton>
       </InfoBox>
 
+        <LeftColumn>Project Description Goes Here</LeftColumn>
         <RightColumn>Additional project details and information go here.</RightColumn>
       </TextColumns>
 
