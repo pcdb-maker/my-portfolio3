1f3e8e5736912d6a19c88c72c60a29bfc3e56179
Chloe O <66051807+pcdb-maker@users.noreply.github.com>
2024-10-05 18:49:30 +1000

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -36,7 +36,6 @@ const Footer = () => {
             paddingTop: '30px'
         }}>
             {/* Banner animation */}
-                    display: 'flex',
                     alignItems: 'center',
                     justifyContent: 'center',
                     width: '100%',
