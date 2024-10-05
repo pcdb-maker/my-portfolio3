88f2e034b340ef0fa5dc94a446cc89fd9066c0a1
Chloe O <66051807+pcdb-maker@users.noreply.github.com>
2024-10-05 18:48:27 +1000

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -99,7 +99,6 @@ const Footer = () => {
                 </div>
 
                 <div style={{ flex: 1, padding: '0 20px' }}>
-                    <h3 style={{ color: '#D79922', marginBottom: '10px' }}>Other Links</h3>
                 <div style={{ padding: '0 20px' }}>
                     <h3 style={{ color: '#D7C3A4' }}>Other Links</h3>
                     <p>Portfolio <br /> Services <br /> About</p>
