51c86d2b725544d5fa276f7bb74e83daa5eaccac
Chloe O <66051807+pcdb-maker@users.noreply.github.com>
2024-10-05 18:50:42 +1000

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -56,7 +56,6 @@ const Footer = () => {
                 <span>Trabajemos juntos</span>
                 <span style={{ margin: '0 20px', fontStyle: 'italic' }}>♦</span>
                 <span>Let's work together</span>
-                <span style={{ margin: '0 20px', fontStyle: 'italic' }}>♦</span>
                 <span>Trabajemos juntos</span>
             </motion.div>
                         width: 'max-content', // Prevents clipping of the text
