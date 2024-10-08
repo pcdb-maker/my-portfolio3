b1f2a484bdd7d199ba9dfbad49647976408230de
Chloe O <66051807+pcdb-maker@users.noreply.github.com>
2024-10-08 16:12:39 +1100

    Update About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -65,6 +65,7 @@ const SpinningIcon = styled(motion.div)`
         I love design in all its forms and enjoy exploring new areas and learning new things every day.
       </BioText>
     </AboutSection>
+              </TableRow>
               <HighlightedRow>
                 <TableData>Feb 2024 - Present</TableData>
                 <TableData>Graphic Designer & Illustrator</TableData>
