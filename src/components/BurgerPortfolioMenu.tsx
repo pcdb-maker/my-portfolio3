756db105714add1f1bce794edf477b9f3acf894f
Chloe O <66051807+pcdb-maker@users.noreply.github.com>
2024-10-08 16:47:03 +1100

    Update BurgerPortfolioMenu.tsx

--- src/components/BurgerPortfolioMenu.tsx
+++ src/components/BurgerPortfolioMenu.tsx
@@ -171,7 +171,6 @@ const BurgerMenu: React.FC = () => {
       <BurgerWrapper onClick={handleClick} visible={visible && !open}>
         <BurgerLines />
       </BurgerWrapper>
-
       <NavbarContainer>
         <Logo>Chloe Osborne <IconWrapper /></Logo>
         <NavLinks>
