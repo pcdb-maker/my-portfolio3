0c2e25ca854c185689d796a572591637d0916e61
Chloe O <66051807+pcdb-maker@users.noreply.github.com>
2024-10-08 16:58:40 +1100

    Update Services.tsx

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -8,6 +8,7 @@ import styled from 'styled-components';
   align-items: center;
   padding: 50px;
   background-color: #C5CBE3;
+  margin: 0; /* Ensure no extra margin */
 `;
 
 const IconContainer = styled.div<{ color?: string }>`
