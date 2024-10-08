66a971ebadce7779e3c95860ea92ccc1c2b945b9
Chloe O <66051807+pcdb-maker@users.noreply.github.com>
2024-10-08 16:55:31 +1100

    Update BurgerPortfolioMenu.tsx

--- src/components/BurgerPortfolioMenu.tsx
+++ src/components/BurgerPortfolioMenu.tsx
@@ -120,7 +120,6 @@ const BurgerMenuContainer = styled.div`
   display: flex;
   flex-direction: column;
   font-family: 'Playfair Display';
-  align-self: flex-end;
   align-items: center;
   justify-content: center;
   z-index: 1000;
