bc388a438323bf60280f7a7d7881b896acb0efb1
Chloe O <66051807+pcdb-maker@users.noreply.github.com>
2024-10-08 16:13:22 +1100

    Update About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -67,6 +67,7 @@ const Table = styled.table`
 const TableRow = styled.tr`
   border-bottom: 1px solid black;
 `;
+  padding: 50px 0;
   return (
     <AboutSection id="about">
       <ProfileImage src="/path/to/profile.jpg" alt="Astrid Mottes" />
