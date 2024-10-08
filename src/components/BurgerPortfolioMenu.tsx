989e4d5523a7ddabd30f56819b4ae88bfb449826
Chloe O <66051807+pcdb-maker@users.noreply.github.com>
2024-10-08 16:49:42 +1100

    Update BurgerPortfolioMenu.tsx

--- src/components/BurgerPortfolioMenu.tsx
+++ src/components/BurgerPortfolioMenu.tsx
@@ -152,7 +152,6 @@ const BurgerMenu: React.FC = () => {
   // Handle click event to toggle menu
   const handleClick = () => {
     setOpen(!open);
-  };
 const CloseButton = styled.div`
   position: absolute;
   top: 20px;
