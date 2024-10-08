f598c008527023eb7ae1fbf46746b4089f358f56
Chloe O <66051807+pcdb-maker@users.noreply.github.com>
2024-10-08 16:13:55 +1100

    Update About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -77,6 +77,7 @@ const TableHeader = styled.th`
 const TableData = styled.td`
   padding: 50px 0;
 
+  useEffect(() => {
     const handleScroll = () => {
       // Calculate rotation based on scroll position, but no position change
       const scrollTop = window.pageYOffset;
