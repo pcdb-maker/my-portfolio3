9f4e785e2747716c9ffd88ada27cdacbf42a2081
Chloe O <66051807+pcdb-maker@users.noreply.github.com>
2024-10-08 16:55:09 +1100

    Update BurgerPortfolioMenu.tsx

--- src/components/BurgerPortfolioMenu.tsx
+++ src/components/BurgerPortfolioMenu.tsx
@@ -114,7 +114,6 @@ const BurgerMenuContainer = styled.div`
   position: fixed;
   top: 0;
   right: 0;
-  border-radius: 15px 15px 15px 15px;
   transform: ${({ open }) => (open ? 'translateX(0)' : 'translateX(100%)')};
   transition: transform 0.5s ease-in-out;
   z-index: 99;
