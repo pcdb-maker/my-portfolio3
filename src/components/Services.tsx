1dbfc4409b832430b228d9d0409281d754ee8572
Chloe O <66051807+pcdb-maker@users.noreply.github.com>
2024-10-08 16:58:17 +1100

    Update Services.tsx

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -22,6 +22,7 @@ const Service = styled(motion.div)`
 
   &:hover {
     transform: translateY(-10px);
+    width: 100%;
     height: 100%;
     path {
       fill: ${({ color }) => color}; /* Apply color to paths */
