15200939321dac3ddc53cb84e32b5c49e44c5e90
Chloe O <66051807+pcdb-maker@users.noreply.github.com>
2024-10-08 16:12:42 +1100

    Update About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -65,6 +65,7 @@ const SpinningIcon = styled(motion.div)`
         I love design in all its forms and enjoy exploring new areas and learning new things every day.
       </BioText>
     </AboutSection>
+                <TableData>Web Designer</TableData>
                 <TableData>t26 Italia</TableData>
               </TableRow>
               <HighlightedRow>
