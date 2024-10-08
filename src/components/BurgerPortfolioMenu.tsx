fb1dcc99af0befab0756e9f56fcb88d3f8c25f46
Chloe O <66051807+pcdb-maker@users.noreply.github.com>
2024-10-08 16:46:09 +1100

    Update BurgerPortfolioMenu.tsx

--- src/components/BurgerPortfolioMenu.tsx
+++ src/components/BurgerPortfolioMenu.tsx
@@ -21,6 +21,7 @@ const BurgerWrapper = styled.div<{ visible: boolean }>`
   justify-content: space-between;
   align-items: center;
   box-shadow: 0 4px 15px rgba(0, 0, 0, 0.3);
+  z-index: 1000;
   cursor: pointer;
   transition: transform 0.3s ease-in-out, background-color 0.3s ease-in-out;
 
