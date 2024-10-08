2480c4dbbb894839a93407b2b7d58a602f7b32e5
Chloe O <66051807+pcdb-maker@users.noreply.github.com>
2024-10-08 16:14:08 +1100

    Update About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -96,6 +96,7 @@ const TableData = styled.td`
         I love design in all its forms and enjoy exploring new areas and learning new things every day.
       </BioText>
     </AboutSection>
+        <TableContainer
           transition={{ duration: 0.8, ease: 'easeOut', delay: 0.5 }}
         >
           <Table>
