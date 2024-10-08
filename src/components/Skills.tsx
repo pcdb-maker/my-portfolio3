12f9ae47ea44cbd8939404ebfa5fd0a1712dca9c
Chloe O <66051807+pcdb-maker@users.noreply.github.com>
2024-10-08 17:00:53 +1100

    Update Skills.tsx

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -23,6 +23,7 @@ const Bar = styled(motion.div)`
 `;
 
 const Progress = styled(motion.div)`
+  height: 100%;
   background-color: #D79922; /* Gold for the progress bar */
   border-radius: 10px;
 `;
