8011001fa5c1401744e4f649ed4599071c4b9f47
Chloe O <66051807+pcdb-maker@users.noreply.github.com>
2024-10-08 16:14:25 +1100

    Update About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -96,6 +96,7 @@ const TableData = styled.td`
         I love design in all its forms and enjoy exploring new areas and learning new things every day.
       </BioText>
     </AboutSection>
+          whileInView={{ opacity: 1, y: 0 }}
           viewport={{ once: true }}
           transition={{ duration: 1, ease: 'easeOut' }}
         >
