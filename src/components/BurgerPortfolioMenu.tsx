b7cdabd52ef4cf2defdf2454be397539dc167661
Chloe O <66051807+pcdb-maker@users.noreply.github.com>
2024-10-08 16:48:16 +1100

    Update BurgerPortfolioMenu.tsx

--- src/components/BurgerPortfolioMenu.tsx
+++ src/components/BurgerPortfolioMenu.tsx
@@ -166,7 +166,6 @@ const BurgerMenu: React.FC = () => {
     };
 
     window.addEventListener('scroll', handleScroll);
-    return () => window.removeEventListener('scroll', handleScroll);
   const toggleBurgerMenu = () => {
     setIsBurgerOpen(!isBurgerOpen);
   };
