03ec3c8d2d7661beaca43b4eedd604dc85bfe9b5
Chloe O <66051807+pcdb-maker@users.noreply.github.com>
2024-10-08 16:58:44 +1100

    Update Services.tsx

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -8,6 +8,7 @@ import styled from 'styled-components';
   align-items: center;
   padding: 50px;
   background-color: #C5CBE3;
+  font-size: 4.5rem;
   font-style: italic;
   letter-spacing: 0.05em;
   margin: 0; /* Ensure no extra margin */
