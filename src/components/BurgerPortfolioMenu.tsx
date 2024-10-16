aae21d8667d9ec701a673a114e48ebd771d88407
Chloe O <66051807+pcdb-maker@users.noreply.github.com>
2024-10-16 19:34:36 +1100

    Media queries

--- src/components/BurgerPortfolioMenu.tsx
+++ src/components/BurgerPortfolioMenu.tsx
@@ -8,7 +8,6 @@ import { Link } from 'react-router-dom';
   border-radius: 15px;
   display: ${({ visible }) => (visible ? 'flex' : 'none')};
   align-items: center;
-  display: flex;
   justify-content: center;
   box-shadow: 0 4px 15px rgba(0, 0, 0, 0.3);
   cursor: pointer;
