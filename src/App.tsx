2036df14e1e911f6cde5c06e52fcb0b3288687b8
Chloe O <66051807+pcdb-maker@users.noreply.github.com>
2024-10-16 19:22:48 +1100

    Media queries

--- src/App.tsx
+++ src/App.tsx
@@ -23,6 +23,7 @@ const ScrollToHash: React.FC = () => {
   if (projectPaths.includes(location.pathname)) {
     return <ProjectNavBar />;
   }
+        delay: 0,
         smooth: 'easeInOutQuart',
       });
     }
