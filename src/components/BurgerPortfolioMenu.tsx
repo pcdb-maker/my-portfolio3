2d27014c10ba2f4db7d3b6c8b57b38d59a6ef681
Chloe O <66051807+pcdb-maker@users.noreply.github.com>
2024-10-16 19:33:50 +1100

    Media queries

--- src/components/BurgerPortfolioMenu.tsx
+++ src/components/BurgerPortfolioMenu.tsx
@@ -10,7 +10,6 @@ import { Link } from 'react-router-dom';
   align-items: center;
   box-sizing: border-box;
   @media (max-width: 768px) {
-const Logo = styled.div`
   font-size: 28px;
 
   display: flex;
