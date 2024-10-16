50a8bade6f77574001045ee4003e6b1dff0f6f02
Chloe O <66051807+pcdb-maker@users.noreply.github.com>
2024-10-16 19:37:09 +1100

    Media queries

--- src/components/BurgerPortfolioMenu.tsx
+++ src/components/BurgerPortfolioMenu.tsx
@@ -130,6 +130,7 @@ const Navbar = () => {
     setIsBurgerOpen(!isBurgerOpen);
   };
 
+    };
 
     window.addEventListener('scroll', handleScroll);
     return () => window.removeEventListener('scroll', handleScroll);
