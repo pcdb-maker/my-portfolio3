92b7f7c82c87709b5904c7e4daf73f317078ac0a
Chloe O <66051807+pcdb-maker@users.noreply.github.com>
2024-10-16 19:33:39 +1100

    Media queries

--- src/components/BurgerPortfolioMenu.tsx
+++ src/components/BurgerPortfolioMenu.tsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 
-  background: #d8c3a5;
   display: flex;
   justify-content: space-between;
   z-index: 150;
