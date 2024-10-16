257502c624e7b391134658575f9679971c2d3e82
Chloe O <66051807+pcdb-maker@users.noreply.github.com>
2024-10-16 19:33:06 +1100

    Media queries

--- src/components/BurgerPortfolioMenu.tsx
+++ src/components/BurgerPortfolioMenu.tsx
@@ -173,6 +173,7 @@ const Navbar = () => {
           <a href="#linkedin">LinkedIn</a>
         </BurgerMenuLinks>
       </BurgerMenuContainer>
+        <div>
         <ExternalLinks href="https://instagram.com" target="_blank">Instagram</ExternalLinks>
         <ExternalLinks href="https://behance.net" target="_blank">Behance</ExternalLinks>
         <ExternalLinks href="https://linkedin.com" target="_blank">LinkedIn</ExternalLinks>
