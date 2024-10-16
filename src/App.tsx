3542698764fa93ccc699f57f765b465b3a843d71
Chloe O <66051807+pcdb-maker@users.noreply.github.com>
2024-10-16 19:21:51 +1100

    Media queries

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,6 @@ const NavbarSelector: React.FC = () => {
   return <Navbar />;
 };
 
-  const projectPaths = ['/project1', '/project2'];
 
   // Conditionally return ProjectNavBar for project pages, otherwise return the standard Navbar
   if (projectPaths.includes(location.pathname)) {
