f229f7c519efab7a0e6d3a23929f6fa607dcbeaa
Chloe O <66051807+pcdb-maker@users.noreply.github.com>
2024-10-16 19:29:19 +1100

    Media queries

--- src/components/BurgerPortfolioMenu.tsx
+++ src/components/BurgerPortfolioMenu.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
 const NavbarContainer = styled.nav`
   top: 0;
