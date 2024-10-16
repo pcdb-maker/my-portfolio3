aa82d34f8608923071d13147a92f6473cd3795df
Chloe O <66051807+pcdb-maker@users.noreply.github.com>
2024-10-16 19:28:13 +1100

    Media queries

--- src/components/BurgerPortfolioMenu.tsx
+++ src/components/BurgerPortfolioMenu.tsx
@@ -1,5 +1,3 @@
-import React, { useState } from 'react';
-import { Link } from 'react-scroll';
 import styled from 'styled-components';
 import { ReactComponent as ClubIcon } from '../assets/club.svg'; // Import Diamond SVG
 
