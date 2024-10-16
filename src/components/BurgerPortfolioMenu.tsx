853d59866506cd5a64d530bc64a265da34ce3656
Chloe O <66051807+pcdb-maker@users.noreply.github.com>
2024-10-16 19:29:17 +1100

    Media queries

--- src/components/BurgerPortfolioMenu.tsx
+++ src/components/BurgerPortfolioMenu.tsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
-import { ReactComponent as ClubIcon } from '../assets/club.svg'; // Import Diamond SVG
 
 const NavbarContainer = styled.nav`
   top: 0;
