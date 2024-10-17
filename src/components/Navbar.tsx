5beada20485cc042dfe7dc426316f77c04250c9a
Chloe O <66051807+pcdb-maker@users.noreply.github.com>
2024-10-17 12:23:18 +1100

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -142,6 +142,7 @@ const Navbar: React.FC = () => {
         <IconWrapper />
       </ContactButton>
     </NavbarContainer>
+          <ExternalLinks href="https://behance.net" target="_blank">Behance</ExternalLinks>
           <ExternalLinks href="https://linkedin.com" target="_blank">LinkedIn</ExternalLinks>
         </div>
       </Sidebar>
