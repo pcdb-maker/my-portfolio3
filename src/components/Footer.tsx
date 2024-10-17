f20ce64b0de78e78cef1270395ecd0beeae5bc2c
Chloe O <66051807+pcdb-maker@users.noreply.github.com>
2024-10-17 12:20:14 +1100

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -87,6 +87,7 @@ const Footer: React.FC = () => {
                     <p>Portfolio <br /> Services <br /> About</p>
                 </div>
             </div>
+                        <LogoSection>
                             <LogoText>Chloe</LogoText>
                             <LogoSubText>OSBORNE</LogoSubText>
                         </LogoSection>
