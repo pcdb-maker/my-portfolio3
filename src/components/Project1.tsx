341219a16996b1a4c735d7da28399976be682da2
Chloe O <66051807+pcdb-maker@users.noreply.github.com>
2024-10-17 12:32:49 +1100

    Update Project1.tsx

--- src/components/Project1.tsx
+++ src/components/Project1.tsx
@@ -278,7 +278,6 @@ const Project1: React.FC = () => {
   return (
     <ProjectContainer>
            <BurgerPortfolioMenu />
-          
       {/* Animated Project Title */}
       <ProjectTitle
         initial={{ y: -50, opacity: 0 }}
