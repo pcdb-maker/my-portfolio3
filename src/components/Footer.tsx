d822cb163e07563c2dc3185003bc68c860c1de0c
Chloe O <66051807+pcdb-maker@users.noreply.github.com>
2024-10-17 12:19:53 +1100

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -89,6 +89,7 @@ const Footer: React.FC = () => {
                     <p>Portfolio <br /> Services <br /> About</p>
                 </div>
             </div>
+                        </FooterTagline>
                     </FooterColumn>
                     <ContactSection>
                         <p className="contact-title">Drop me a line</p>
