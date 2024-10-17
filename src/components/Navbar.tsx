6fe96195777c5684cbf9c78aa319f666e5af246c
Chloe O <66051807+pcdb-maker@users.noreply.github.com>
2024-10-17 12:23:39 +1100

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -142,6 +142,7 @@ const Navbar: React.FC = () => {
         <IconWrapper />
       </ContactButton>
     </NavbarContainer>
+        ) : (
           <>
             <RouterInternalLinks to="/#portfolioHeader">Portfolio</RouterInternalLinks>
             <RouterInternalLinks to="/#services">Services</RouterInternalLinks>
