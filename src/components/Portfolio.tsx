a9dff724496d4836848d24e7a5a9d2f1d26f3a64
Chloe O <66051807+pcdb-maker@users.noreply.github.com>
2024-10-17 12:35:10 +1100

    Update Portfolio.tsx

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -150,11 +150,6 @@ const Portfolio: React.FC = () => {
         </ProjectCard>
 
         {/* Second Card */}
-        <ProjectCard large={false} style={{ y: y2 }} transition={{ ease: [0.25, 0.1, 0.25, 1], duration: 0.01 }}>
-          <Link to="/project2">
-            <ParallaxImage src="https://static.wixstatic.com/media/36e847_9feb77b2a07c4d749bbcca75887be7ee~mv2.webp" alt="project2" />
-            <CardTitle>Opere d'Olio</CardTitle>
-            <Subtitle>Packaging Design</Subtitle>
         <ProjectCard
           large={false}
           style={isMobile ? {} : { y: y2 }}
