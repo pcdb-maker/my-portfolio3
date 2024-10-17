861decfe29c11df10488a57a537b532a5dcc427c
Chloe O <66051807+pcdb-maker@users.noreply.github.com>
2024-10-17 12:34:54 +1100

    Update Portfolio.tsx

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -175,11 +175,10 @@ const Portfolio: React.FC = () => {
             <ParallaxImage src="https://static.wixstatic.com/media/36e847_9feb77b2a07c4d749bbcca75887be7ee~mv2.webp" alt="project2" />
             <CardTitle>Opere d'Olio</CardTitle>
             <Subtitle>Packaging Design</Subtitle>
-            <CircleLink className="CircleLink">→</CircleLink>
+            <CircleLink className="CircleLink">&rarr;</CircleLink>
           </Link>
         </ProjectCard>
 
-         {/* fith Card */}
         {/* More Cards (3rd to 6th) */}
         {[y3, y4, y5, y6].map((yTransform, index) => (
           <ProjectCard
