52db0ce6bd680e2bab6dacaae64f991d5665d391
Chloe O <66051807+pcdb-maker@users.noreply.github.com>
2024-10-17 12:17:33 +1100

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -149,6 +149,7 @@ const Footer: React.FC = () => {
 export default Footer;
 
 // Styled-components for better responsiveness and animations
+    color: #ffffff;
     flex-direction: column;
     justify-content: space-between;
     min-height: 85vh;
