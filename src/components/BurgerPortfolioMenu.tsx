48d9664fc8ffb3b9888c3a894ccf443242fb2300
Chloe O <66051807+pcdb-maker@users.noreply.github.com>
2024-10-17 12:14:06 +1100

    Update BurgerPortfolioMenu.tsx

--- src/components/BurgerPortfolioMenu.tsx
+++ src/components/BurgerPortfolioMenu.tsx
@@ -86,6 +86,7 @@ const BurgerMenuContainer = styled.div`
   width: 100%;
   height: 100vh;
   background-color: #2f2f2f;
+  z-index: 99;
   display: flex;
   flex-direction: column;
   justify-content: space-between;
