a8f25c565a99e62be140842175ae9e42d55a7855
Chloe O <66051807+pcdb-maker@users.noreply.github.com>
2024-10-17 12:12:38 +1100

    Update BurgerPortfolioMenu.tsx

--- src/components/BurgerPortfolioMenu.tsx
+++ src/components/BurgerPortfolioMenu.tsx
@@ -120,6 +120,7 @@ const CloseButton = styled.div`
   font-size: 24px;
   color: #e85a4f;
   cursor: pointer;
+  text-decoration: none;
   margin-top: 1px;
   margin-bottom: 1px;
   margin-left: 25px;
