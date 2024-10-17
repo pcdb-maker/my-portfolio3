504f6cef322ef7b125ffac3864718ac5f550e21f
Chloe O <66051807+pcdb-maker@users.noreply.github.com>
2024-10-17 12:18:44 +1100

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -42,7 +42,6 @@ const Footer: React.FC = () => {
     return (
         <FooterContainer id="footer">
             {/* Banner animation */}
-            }}>
                 <div
                     ref={scrollContainerRef}
                     style={{
