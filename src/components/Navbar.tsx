898e2640b5dc232f922b8621c4a770ba07f75afb
Chloe O <66051807+pcdb-maker@users.noreply.github.com>
2024-10-17 12:24:11 +1100

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -157,6 +157,7 @@ const Navbar: React.FC = () => {
         <IconWrapper />
       </ContactButton>
     </NavbarContainer>
+      </BurgerWrapper>
 
       <Sidebar open={open} topMenuActive={topMenuActive} burgerPosition={burgerPosition}>
         <CloseButton onClick={() => toggleBurgerMenu(topMenuActive)}> X </CloseButton>
