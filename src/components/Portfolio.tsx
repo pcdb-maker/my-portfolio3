5b44e55379d07153dca20c7ae7ae20cd37ff42c7
Chloe O <66051807+pcdb-maker@users.noreply.github.com>
2024-10-17 12:29:47 +1100

    Update Portfolio.tsx

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -194,6 +194,7 @@ const Portfolio: React.FC = () => {
 
 
 
+            large={index % 2 === 0}
             style={isMobile ? {} : { y: yTransform }}
             transition={{ ease: [0.25, 0.1, 0.25, 1], duration: 0.01 }}
           >
