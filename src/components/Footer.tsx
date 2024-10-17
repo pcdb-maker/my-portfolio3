e9383f792408024022a1e0773d6a2e580b0b67ef
Chloe O <66051807+pcdb-maker@users.noreply.github.com>
2024-10-17 12:19:44 +1100

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -89,6 +89,7 @@ const Footer: React.FC = () => {
                     <p>Portfolio <br /> Services <br /> About</p>
                 </div>
             </div>
+                            chloeprofessionaldevelopment@outlook.com
                         </a>
                     </ContactSection>
                     <FooterLinks>
