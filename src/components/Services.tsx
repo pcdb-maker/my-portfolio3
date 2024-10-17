9c2c08a3b71565485157bb7f2c24465dcbe8cac4
Chloe O <66051807+pcdb-maker@users.noreply.github.com>
2024-10-17 12:31:06 +1100

    Update Services.tsx

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -12,6 +12,7 @@ const ServicesContainer = styled.div`
   color: white;
   text-align: center;
   padding: 100px 20px;
+/* For Larger Laptops and Desktops: */
 @media (min-width: 1441px) {
   body {
     height: 100vh;
