2d747ff5dfe1d43c46c78d513a4884bd9453b220
Chloe O <66051807+pcdb-maker@users.noreply.github.com>
2024-10-17 12:13:54 +1100

    Update BurgerPortfolioMenu.tsx

--- src/components/BurgerPortfolioMenu.tsx
+++ src/components/BurgerPortfolioMenu.tsx
@@ -93,6 +93,7 @@ const BurgerMenuContainer = styled.div`
   z-index: 1000;
   transition: transform 0.3s ease;
   transform: ${({ isOpen }) => (isOpen ? 'translateX(0)' : 'translateX(100%)')};
+  color: white;
 `;
 
 const CloseButton = styled.div`
