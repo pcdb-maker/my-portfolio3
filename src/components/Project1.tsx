ce8c0f453c6a2f5e3a402c60fc44d7e69cbc0081
Chloe O <66051807+pcdb-maker@users.noreply.github.com>
2024-10-17 12:34:29 +1100

    Update Project1.tsx

--- src/components/Project1.tsx
+++ src/components/Project1.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import styled from 'styled-components';
-import { motion, useTransform } from 'framer-motion';
-import { useInView } from 'react-intersection-observer'; 
 import { Link } from 'react-router-dom';
 
 // Main container for the project page
