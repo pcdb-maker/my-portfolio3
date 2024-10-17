f89f9748e6ca15cc89a1cbe2020b8b77e86fd678
Chloe O <66051807+pcdb-maker@users.noreply.github.com>
2024-10-17 12:19:11 +1100

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -83,6 +83,7 @@ const Footer: React.FC = () => {
                     <p>Portfolio <br /> Services <br /> About</p>
                 </div>
             </div>
+                        <ul>
                             <li>Instagram</li>
                             <li>Behance</li>
                             <li>LinkedIn</li>
