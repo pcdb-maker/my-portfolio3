8e4eba83338cdc574a21601c577f024b6cba2af7
Chloe O <66051807+pcdb-maker@users.noreply.github.com>
2024-10-17 12:29:36 +1100

    Update Portfolio.tsx

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -194,6 +194,7 @@ const Portfolio: React.FC = () => {
 
 
 
+                src="https://static.wixstatic.com/media/36e847_9feb77b2a07c4d749bbcca75887be7ee~mv2.webp"
               />
               <CardTitle>Opere d'Olio</CardTitle>
               <Subtitle>Packaging Design</Subtitle>
