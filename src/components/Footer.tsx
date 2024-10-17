584f6b2e5f1acbafe29bcebdca76f925cbad96aa
Chloe O <66051807+pcdb-maker@users.noreply.github.com>
2024-10-17 12:18:28 +1100

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -40,7 +40,6 @@ const Footer: React.FC = () => {
     }, []);
 
     return (
-            justifyContent: 'space-between', 
             minHeight: '85vh', 
             position: 'relative',
             paddingTop: '30px'
