a31af8737a9c22e233373c1b6a122fe437d32527
Chloe O <66051807+pcdb-maker@users.noreply.github.com>
2024-10-17 12:31:54 +1100

    Update Services.tsx

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -15,6 +15,7 @@ const ServicesContainer = styled.div`
   
   
 
+ @media (max-width: 375px) {
 /* For Larger Phones/Phablets: Galaxy Note, iPhone 13 Pro Max */
 @media (min-width: 415px) and (max-width: 768px) {
   body {
