5d459f418198138b8e4b8c6c3f199e16836d8954
Chloe O <66051807+pcdb-maker@users.noreply.github.com>
2024-10-17 12:18:23 +1100

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -40,7 +40,6 @@ const Footer: React.FC = () => {
     }, []);
 
     return (
-            fontFamily: 'Playfair Display',
             color: '#ffffff', 
             display: 'flex', 
             flexDirection: 'column', 
